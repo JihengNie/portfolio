542a3573bc4d8051c771ddf163c07e8084264915
Jiheng Nie <Niejiheng@gmail.com>
2022-10-07 23:35:42 +0000

    In progress

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// var navHome = document.querySelector('.nav-home');
+// var navAbout = document.querySelector('.nav-about');
+// var navMath = document.querySelector('.nav-math');
+// var navProjects = document.querySelector('.nav-projects');
+// var navContact = document.querySelector('.nav-contacts');
